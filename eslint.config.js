90598678c17e034e72deea7741dd376abfbc0fcb
hezhihang <hezhihang@baidu.com>
2025-12-24 20:27:07 +0800

    chore: 框架搭建

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  { ignores: ['dist', 'node_modules', '.turbo', 'build'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2025,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2024
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules
+    }
+  },
+  prettier
+];
